97c5662818bbfccc567119227ea06901f0470f27
Kamarudin <rudi.krsoftware@gmail.com>
2017-01-04 14:14:32 +0700

    Penambahan class ModelExtension

---
+++ src/OpenRetail.Model/ModelExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dapper.Contrib.Extensions;
+
+namespace OpenRetail.Model
+{
+    public static class ModelExtension
+    {
+        /// <summary>
+        /// Method untuk mendapatkan nama tabel dari class model
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string GetTableName<T>(this T obj)
+        {
+            var tableName = string.Empty;
+            var type = typeof(T);
+
+            // Get instance of the attribute.
+            var tableAttribute = (TableAttribute)Attribute.GetCustomAttribute(type, typeof(TableAttribute));
+
+            if (!(tableAttribute == null))
+                tableName = tableAttribute.Name;
+
+            return tableName;
+        }
+    }
+}
