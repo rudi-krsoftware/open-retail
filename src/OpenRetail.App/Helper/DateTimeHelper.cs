76cae89462b7109bc139458aa8f14a579847d617
Kamarudin <rudi.krsoftware@gmail.com>
2017-01-09 14:52:28 +0700

    Penambahan class helper

---
+++ src/OpenRetail.App/Helper/DateTimeHelper.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenRetail.App.Helper
+{
+    public static class DateTimeHelper
+    {
+
+        public static DateTime GetNullDateTime()
+        {
+            return new DateTime(0001, 1, 1);
+        }
+
+        /// <summary>
+        /// Method untuk mengecek apakah sebuah tanggal null atau tidak
+        /// </summary>
+        /// <param name="tanggal"></param>
+        /// <returns></returns>
+        public static bool IsNull(DateTime tanggal)
+        {
+            var result = true;
+
+            try
+            {
+                result = tanggal == DateTime.MinValue || tanggal == new DateTime(1753, 1, 1) ||
+                         tanggal == new DateTime(0001, 1, 1);
+            }
+            catch
+            {
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Untuk mengkonversi data tanggal ke format dd/MM/yyyy
+        /// </summary>
+        /// <param name="tanggal">Data tanggal dengan tipe DateTime</param>
+        /// <returns></returns>
+        public static string DateToString(DateTime tanggal, string format = "dd/MM/yyyy")
+        {
+            var result = string.Empty;
+
+            try
+            {
+                if (!(tanggal == DateTime.MinValue || tanggal == new DateTime(1753, 1, 1)))
+                    result = string.Format("{0:" + format + "}", tanggal);
+            }
+            catch
+            {
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Untuk mengkonversi data tanggal ke format hari bulan tahun
+        /// </summary>
+        /// <param name="hari"></param>
+        /// <param name="bulan"></param>
+        /// <param name="tahun"></param>
+        /// <returns></returns>
+        public static string DateToString(int hari, int bulan, int tahun)
+        {
+            string result = hari + " " + DayMonthHelper.GetBulanIndonesia(bulan) + " " + tahun;
+
+            return result;
+        }
+
+        public static string TimeToString(DateTime jam, string format = "HH:mm:ss")
+        {
+            string result = jam.ToString(format);
+
+            return result;
+        }
+
+        public static DateTime GetTime(int hour, int minute, int second = 0)
+        {
+            return new DateTime(1753, 1, 1, hour, minute, second);
+        }
+
+        public static DateTime GetTime(string jam)
+        {
+            var words = jam.Split(':');
+            var time = new DateTime(1753, 1, 1, int.Parse(words[0]), int.Parse(words[1]), 0);
+
+            return time;
+        }
+
+        /// <summary>
+        /// Untuk mengkonversi data string ke format tanggal
+        /// </summary>
+        /// <param name="tanggal">Data tanggal dengan format dd/MM/yyyy</param>
+        /// <returns></returns>
+        public static DateTime StringToDateTime(string tanggal)
+        {
+            var words = tanggal.Split('/');
+
+            var tgl = new DateTime(int.Parse(words[2]), int.Parse(words[1]), int.Parse(words[0]));
+
+            return tgl;
+        }
+
+        public static bool IsValidTimeValue(string jam)
+        {
+            var result = false;
+
+            try
+            {
+                var words = jam.Split(':');
+                var time = new DateTime(1753, 1, 1, int.Parse(words[0]), int.Parse(words[1]), 0);
+
+                result = true;
+            }
+            catch
+            {
+            }
+
+            return result;
+        }
+
+        public static bool IsValidRangeTanggal(DateTime tanggalMulai, DateTime tanggalSelesai)
+        {
+            return tanggalMulai <= tanggalSelesai;
+        }
+    }
+}
---
+++ src/OpenRetail.App/Helper/DayMonthHelper.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenRetail.App.Helper
+{
+    public static class DayMonthHelper
+    {
+
+        /// <summary>
+        /// Untuk mengecek tanggal minimal
+        /// </summary>
+        /// <param name="tanggal"></param>
+        /// <returns></returns>
+        public static bool IsMinDate(DateTime tanggal)
+        {
+            try
+            {
+                return (tanggal == DateTime.MinValue || tanggal == new DateTime(1753, 1, 1));
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Untuk mendapatkan index hari
+        /// </summary>
+        /// <param name="tanggal">Tanggal yang ingin dicari index harinya</param>
+        /// <returns></returns>
+        private static int Weekday(DateTime tanggal)
+        {
+            DayOfWeek startOfWeek = DayOfWeek.Sunday;
+            return (tanggal.DayOfWeek - startOfWeek + 7) % 7;
+        }
+
+        /// <summary>
+        /// Untuk mendapatkan nama hari berdasarkan tanggal
+        /// </summary>
+        /// <param name="tanggal">Tanggal yang ingin dicari harinya</param>
+        /// <returns></returns>
+        public static string GetHariIndonesia(DateTime tanggal)
+        {
+            string[] hari = { "Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu" };
+
+            return hari[Weekday(tanggal)];
+        }
+
+        /// <summary>
+        /// Untuk mendapatkan nama bulan dalam format huruf berdasarkan bulan angka
+        /// </summary>
+        /// <param name="bulan">Diisi dengan bulan angka</param>
+        /// <returns></returns>
+        public static string GetBulanIndonesia(int bulan)
+        {
+            string[] bulans = { 
+                                "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", 
+                                "Agustus", "September", "Oktober", "November", "Desember" 
+                              };
+
+            return bulans[bulan - 1];
+        }
+
+        /// <summary>
+        /// Untuk mendapatkan index bulan berdasarkan nama bulan
+        /// </summary>
+        /// <param name="bulan">Diisi dengan nama bulan. Misal Januari</param>
+        /// <returns></returns>
+        public static int GetBulanAngka(string bulan)
+        {
+            Dictionary<string, string> daftarBulan = new Dictionary<string, string>();
+
+            daftarBulan.Clear();
+            daftarBulan.Add("Januari", "1");
+            daftarBulan.Add("Februari", "2");
+            daftarBulan.Add("Maret", "3");
+            daftarBulan.Add("April", "4");
+            daftarBulan.Add("Mei", "5");
+            daftarBulan.Add("Juni", "6");
+            daftarBulan.Add("Juli", "7");
+            daftarBulan.Add("Agustus", "8");
+            daftarBulan.Add("September", "9");
+            daftarBulan.Add("Oktober", "10");
+            daftarBulan.Add("November", "11");
+            daftarBulan.Add("Desember", "12");
+
+            return Convert.ToInt32(daftarBulan[StringHelper.Split(bulan, 0)]);
+        }
+
+        /// <summary>
+        /// Untuk mendapatkan informasi daftar bulan dan tahun
+        /// </summary>
+        /// <param name="tahunMulai">Diisi dengan tahun mulai, nilai default 2011</param>
+        /// <param name="isBulanOnly">Diisi dengan nilai true atau false, nilai default false</param>
+        /// <returns></returns>
+        public static List<string> GetListBulan(int tahunMulai = 2011, bool isBulanOnly = false)
+        {
+            var listBulan = new List<string>();
+
+            for (int tahun = tahunMulai; tahun <= DateTime.Today.Year; tahun++)
+            {
+                for (int bulan = 1; bulan < 13; bulan++)
+                {
+                    if (isBulanOnly)
+                    {
+                        listBulan.Add(GetBulanIndonesia(bulan));
+                    }
+                    else
+                    {
+                        listBulan.Add(GetBulanIndonesia(bulan) + " " + tahun);
+                    }
+                }
+            }
+
+            return listBulan;
+        }
+    }
+}
