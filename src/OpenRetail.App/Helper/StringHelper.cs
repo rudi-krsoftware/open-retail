f3f3d107d7d987dff539253158e73856dd667770
Kamarudin <rudi.krsoftware@gmail.com>
2017-01-10 11:30:33 +0700

    Penambahan class StringHelper

---
+++ src/OpenRetail.App/Helper/StringHelper.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace OpenRetail.App.Helper
+{
+    public static class StringHelper
+    {
+        public static char ConvertToUpper(System.Windows.Forms.KeyPressEventArgs e)
+        {
+            return Convert.ToChar(e.KeyChar.ToString().ToUpper());
+        }
+
+        public static void SendStringToText(string data, string fileName)
+        {
+            using (var writer = new StreamWriter(fileName, false))
+            {
+                writer.WriteLine(data);
+            }
+        }
+
+        public static string PrintChar(char karakter, int jumlahKarakter)
+        {
+            var result = string.Empty;
+
+            for (int i = 0; i < jumlahKarakter; i++)
+            {
+                result += karakter;
+            }
+
+            return result;
+        }
+
+        public static string FixedLength(string s, int fixedLength)
+        {
+            if (s.Length > fixedLength)
+                s = s.Substring(0, fixedLength);
+
+            var result = s;
+            var sLength = s.Length;
+
+            var sisa = fixedLength - sLength;
+
+            for (int i = 0; i < sisa; i++)
+            {
+                result += " ";
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Untuk menampilkan string dengan format rata kanan
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="fixedLength"></param>
+        /// <returns></returns>
+        public static string RightAlignment(string s, int fixedLength)
+        {
+            if (s.Length > fixedLength)
+                s = s.Substring(0, fixedLength);
+
+            var result = s;
+
+            var sLength = s.Length;
+            var sisa = fixedLength - sLength;
+
+            var space = string.Empty;
+            for (int i = 0; i < sisa; i++)
+            {
+                space += " ";
+            }
+
+            result = space + result;
+
+            return result;
+        }
+
+        /// <summary>
+        /// Fungsi untuk memecah string berdasarkan karakter spasi
+        /// </summary>
+        /// <param name="s">string yang ingin dipecah</param>
+        /// <param name="index">index string yang diingin diambil</param>
+        /// <returns></returns>
+        public static string Split(string s, int index)
+        {
+            string[] words = s.Split(' ');
+
+            words[index] = words[index].ToString().TrimStart();
+            words[index] = words[index].ToString().TrimEnd();
+
+            return words[index];
+        }
+
+        /// <summary>
+        /// Fungsi untuk memecah string berdasarkan karakter yang diinginkan
+        /// </summary>
+        /// <param name="s">string yang ingin dipecah</param>
+        /// <param name="index">index string yang diingin diambil</param>
+        /// <param name="separator">karakter pemisah string</param>
+        /// <returns></returns>
+        public static string Split(string s, short index, char separator)
+        {
+            string[] words = s.Split(separator);
+
+            words[index] = words[index].ToString().TrimStart();
+            words[index] = words[index].ToString().TrimEnd();
+
+            return words[index];
+        }
+    }
+}
