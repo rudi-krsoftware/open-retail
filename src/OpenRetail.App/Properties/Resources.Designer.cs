7353bb2358955d2edd210b8c62db36e8f97e7469
Kamarudin <rudi.krsoftware@gmail.com>
2017-01-11 20:31:23 +0700

    Penambahan resource icon

--- src/OpenRetail.App/Properties/Resources.Designer.cs
+++ src/OpenRetail.App/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace OpenRetail.App.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap search16 {
+            get {
+                object obj = ResourceManager.GetObject("search16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
