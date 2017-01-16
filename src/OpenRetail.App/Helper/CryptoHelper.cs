1a5eed66151dcb7f2168d63f62494a279a5d8b19
Kamarudin <rudi.krsoftware@gmail.com>
2017-01-16 21:40:51 +0700

    Penambahan class CryptoHelper untuk mengenkripsi password

---
+++ src/OpenRetail.App/Helper/CryptoHelper.cs
@@ -0,0 +1,148 @@
+﻿/**
+ * Copyright (C) 2017 Kamarudin (http://coding4ever.net/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * The latest version of this file can be found at https://github.com/rudi-krsoftware/open-retail
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace OpenRetail.App.Helper
+{
+    public static class CryptoHelper
+    {
+        /// <summary>
+        /// Untuk mengenkripsi string menggunakan algoritma MD5
+        /// </summary>
+        /// <param name="plainText">String yang ingin dienkripsi</param>
+        /// <returns></returns>
+        public static string GetMD5Hash(string plainText, string key = "")
+        {
+            if (key.Length > 0)
+                plainText += key;
+
+            var x = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            var bs = System.Text.Encoding.UTF8.GetBytes(plainText);
+            bs = x.ComputeHash(bs);
+
+            var s = new System.Text.StringBuilder();
+            foreach (byte b in bs)
+            {
+                s.Append(b.ToString("x2").ToLower());
+            }
+            string password = s.ToString();
+            return password;
+        }
+
+        #region TripleDES Crypto
+
+        /// <summary>
+        /// Untuk mengenkripsi string menggunakan algoritma TripleDES
+        /// </summary>
+        /// <param name="plainText"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string GetTripleDESEncryptHash(string plainText, string key)
+        {
+            byte[] Results;
+            var UTF8 = new System.Text.UTF8Encoding();
+
+            // Step 1. We hash the passphrase using MD5
+            // We use the MD5 hash generator as the result is a 128 bit byte array
+            // which is a valid length for the TripleDES encoder we use below
+
+            var HashProvider = new MD5CryptoServiceProvider();
+            byte[] TDESKey = HashProvider.ComputeHash(UTF8.GetBytes(key));
+
+            // Step 2. Create a new TripleDESCryptoServiceProvider object
+            var TDESAlgorithm = new TripleDESCryptoServiceProvider();
+
+            // Step 3. Setup the encoder
+            TDESAlgorithm.Key = TDESKey;
+            TDESAlgorithm.Mode = CipherMode.ECB;
+            TDESAlgorithm.Padding = PaddingMode.PKCS7;
+
+            // Step 4. Convert the input string to a byte[]
+            var DataToEncrypt = UTF8.GetBytes(plainText);
+
+            // Step 5. Attempt to encrypt the string
+            try
+            {
+                ICryptoTransform Encryptor = TDESAlgorithm.CreateEncryptor();
+                Results = Encryptor.TransformFinalBlock(DataToEncrypt, 0, DataToEncrypt.Length);
+            }
+            finally
+            {
+                // Clear the TripleDes and Hashprovider services of any sensitive information
+                TDESAlgorithm.Clear();
+                HashProvider.Clear();
+            }
+
+            // Step 6. Return the encrypted string as a base64 encoded string
+            return Convert.ToBase64String(Results);
+        }
+
+        /// <summary>
+        /// Untuk mendekripsi enkrip string menggunakan algoritma TripleDES
+        /// </summary>
+        /// <param name="encryptText"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string GetTripleDESDecryptHash(string encryptText, string key)
+        {
+            byte[] Results;
+            var UTF8 = new System.Text.UTF8Encoding();
+
+            // Step 1. We hash the passphrase using MD5
+            // We use the MD5 hash generator as the result is a 128 bit byte array
+            // which is a valid length for the TripleDES encoder we use below
+
+            var HashProvider = new MD5CryptoServiceProvider();
+            byte[] TDESKey = HashProvider.ComputeHash(UTF8.GetBytes(key));
+
+            // Step 2. Create a new TripleDESCryptoServiceProvider object
+            var TDESAlgorithm = new TripleDESCryptoServiceProvider();
+
+            // Step 3. Setup the decoder
+            TDESAlgorithm.Key = TDESKey;
+            TDESAlgorithm.Mode = CipherMode.ECB;
+            TDESAlgorithm.Padding = PaddingMode.PKCS7;
+
+            // Step 4. Convert the input string to a byte[]
+            var DataToDecrypt = Convert.FromBase64String(encryptText);
+
+            // Step 5. Attempt to decrypt the string
+            try
+            {
+                ICryptoTransform Decryptor = TDESAlgorithm.CreateDecryptor();
+                Results = Decryptor.TransformFinalBlock(DataToDecrypt, 0, DataToDecrypt.Length);
+            }
+            finally
+            {
+                // Clear the TripleDes and Hashprovider services of any sensitive information
+                TDESAlgorithm.Clear();
+                HashProvider.Clear();
+            }
+
+            // Step 6. Return the decrypted string in UTF8 format
+            return UTF8.GetString(Results);
+        }
+
+        #endregion
+    }
+}
