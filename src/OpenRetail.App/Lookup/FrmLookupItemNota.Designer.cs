cbb1f6d9134b54463d378a12d1e428e4efc15b24
Kamarudin <rudi.krsoftware@gmail.com>
2017-01-24 12:46:21 +0700

    Penambahan form lookup untuk menampilkan item nota pembelian/penjualan

---
+++ src/OpenRetail.App/Lookup/FrmLookupItemNota.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace OpenRetail.App.Lookup
+{
+    partial class FrmLookupItemNota
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.gridList = new Syncfusion.Windows.Forms.Grid.GridListControl();
+            this.tableLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.gridList, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 41);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(568, 404);
+            this.tableLayoutPanel3.TabIndex = 0;
+            // 
+            // gridList
+            // 
+            this.gridList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(235)))), ((int)(((byte)(242)))));
+            this.gridList.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.gridList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridList.ItemHeight = 17;
+            this.gridList.Location = new System.Drawing.Point(3, 3);
+            this.gridList.MultiColumn = false;
+            this.gridList.Name = "gridList";
+            this.gridList.Properties.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.gridList.Properties.ForceImmediateRepaint = false;
+            this.gridList.Properties.MarkColHeader = false;
+            this.gridList.Properties.MarkRowHeader = false;
+            this.gridList.SelectedIndex = -1;
+            this.gridList.Size = new System.Drawing.Size(562, 398);
+            this.gridList.TabIndex = 0;
+            this.gridList.TopIndex = 0;
+            this.gridList.DoubleClick += new System.EventHandler(this.gridList_DoubleClick);
+            this.gridList.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.gridList_KeyPress);
+            // 
+            // FrmLookupItemNota
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 486);
+            this.Controls.Add(this.tableLayoutPanel3);
+            this.Name = "FrmLookupItemNota";
+            this.Text = "FrmLookupItemNota";
+            this.Controls.SetChildIndex(this.tableLayoutPanel3, 0);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private Syncfusion.Windows.Forms.Grid.GridListControl gridList;
+    }
+}
---
+++ src/OpenRetail.App/Lookup/FrmLookupItemNota.cs
@@ -0,0 +1,232 @@
+﻿using OpenRetail.App.Helper;
+using OpenRetail.App.UI.Template;
+using OpenRetail.Model;
+using Syncfusion.Windows.Forms.Grid;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OpenRetail.App.Lookup
+{
+    public partial class FrmLookupItemNota : FrmLookupEmptyBody
+    {
+        private IList<ItemJualProduk> _listOfItemJual = null;
+        private IList<ItemBeliProduk> _listOfItemBeli = null;
+
+        private ReferencesType _referensiType = ReferencesType.Produk;
+
+        public IListener Listener { private get; set; }
+
+        public FrmLookupItemNota(string header, IList<ItemJualProduk> listOfItemJual)
+            : base()
+        {
+            InitializeComponent();
+
+            base.SetHeader(header);
+            this._listOfItemJual = listOfItemJual;
+            this._referensiType = ReferencesType.NotaJualProduk;
+
+            InitGridList();
+            base.SetActiveBtnPilih(listOfItemJual.Count > 0);
+        }
+
+        public FrmLookupItemNota(string header, IList<ItemBeliProduk> listOfItemBeli)
+            : base()
+        {
+            InitializeComponent();
+
+            base.SetHeader(header);
+            this._listOfItemBeli = listOfItemBeli;
+            this._referensiType = ReferencesType.NotaBeliProduk;
+
+            InitGridList();
+            base.SetActiveBtnPilih(listOfItemBeli.Count > 0);
+        }
+
+        private void InitGridList()
+        {
+            var gridListProperties = new List<GridListControlProperties>();
+
+            switch (_referensiType)
+            {
+                case ReferencesType.NotaJualProduk:
+                case ReferencesType.NotaBeliProduk:
+                    gridListProperties.Add(new GridListControlProperties { Header = "No", Width = 30 });
+                    gridListProperties.Add(new GridListControlProperties { Header = "Nama Produk", Width = 300 });
+                    gridListProperties.Add(new GridListControlProperties { Header = "Jumlah", Width = 50 });
+                    gridListProperties.Add(new GridListControlProperties { Header = "Harga", Width = 90 });
+                    gridListProperties.Add(new GridListControlProperties { Header = "Sub Total", Width = 90 });
+
+                    break;
+
+                default:
+                    break;
+            }
+
+            var listCount = 0;
+
+            switch (_referensiType)
+            {
+                case ReferencesType.NotaJualProduk:
+                    GridListControlHelper.InitializeGridListControl<ItemJualProduk>(this.gridList, _listOfItemJual, gridListProperties);
+                    this.gridList.Grid.QueryCellInfo += GridItemJualProduk_QueryCellInfo;
+
+                    listCount = _listOfItemJual.Count;
+
+                    break;
+
+                case ReferencesType.NotaBeliProduk:
+                    GridListControlHelper.InitializeGridListControl<ItemBeliProduk>(this.gridList, _listOfItemBeli, gridListProperties);
+                    this.gridList.Grid.QueryCellInfo += GridItemBeliProduk_QueryCellInfo;
+
+                    listCount = _listOfItemBeli.Count;
+
+                    break;
+
+                default:
+                    break;
+            }
+
+            if (listCount > 0)
+                this.gridList.SetSelected(0, true);
+        }
+
+        private void GridItemBeliProduk_QueryCellInfo(object sender, Syncfusion.Windows.Forms.Grid.GridQueryCellInfoEventArgs e)
+        {
+            if (_listOfItemBeli.Count > 0)
+            {
+                if (e.RowIndex > 0)
+                {
+                    var rowIndex = e.RowIndex - 1;
+
+                    if (rowIndex < _listOfItemBeli.Count)
+                    {
+                        var obj = _listOfItemBeli[rowIndex];
+                        var produk = obj.Produk;
+
+                        switch (e.ColIndex)
+                        {
+                            case 2:
+                                if (produk != null)
+                                    e.Style.CellValue = produk.nama_produk;
+                                break;
+
+                            case 3:
+                                e.Style.HorizontalAlignment = GridHorizontalAlignment.Center;
+                                e.Style.CellValue = obj.jumlah - obj.jumlah_retur;
+                                break;
+
+                            case 4:
+                                e.Style.HorizontalAlignment = GridHorizontalAlignment.Right;
+                                e.Style.CellValue = NumberHelper.NumberToString(obj.harga);
+                                break;
+
+                            case 5:
+                                e.Style.HorizontalAlignment = GridHorizontalAlignment.Right;
+                                e.Style.CellValue = NumberHelper.NumberToString((obj.jumlah - obj.jumlah_retur) * obj.harga);
+                                break;
+
+                            default:
+                                break;
+                        }
+
+                        // we handled it, let the grid know
+                        e.Handled = true;
+                    }
+                }
+            }
+        }
+
+        private void GridItemJualProduk_QueryCellInfo(object sender, Syncfusion.Windows.Forms.Grid.GridQueryCellInfoEventArgs e)
+        {
+            if (_listOfItemJual.Count > 0)
+            {
+                if (e.RowIndex > 0)
+                {
+                    var rowIndex = e.RowIndex - 1;
+
+                    if (rowIndex < _listOfItemJual.Count)
+                    {
+                        var obj = _listOfItemJual[rowIndex];
+                        var produk = obj.Produk;
+
+                        switch (e.ColIndex)
+                        {
+                            case 2:
+                                if (produk != null)
+                                    e.Style.CellValue = produk.nama_produk;
+                                break;
+
+                            case 3:
+                                e.Style.HorizontalAlignment = GridHorizontalAlignment.Center;
+                                e.Style.CellValue = obj.jumlah - obj.jumlah_retur;
+                                break;
+
+                            case 4:
+                                e.Style.HorizontalAlignment = GridHorizontalAlignment.Right;
+                                e.Style.CellValue = NumberHelper.NumberToString(obj.harga_jual);
+                                break;
+
+                            case 5:
+                                e.Style.HorizontalAlignment = GridHorizontalAlignment.Right;
+                                e.Style.CellValue = NumberHelper.NumberToString((obj.jumlah - obj.jumlah_retur) * obj.harga_jual);
+                                break;
+
+                            default:
+                                break;
+                        }
+
+                        // we handled it, let the grid know
+                        e.Handled = true;
+                    }
+                }
+            }
+        }
+
+        protected override void Pilih()
+        {
+            var rowIndex = this.gridList.SelectedIndex;
+
+            if (!base.IsSelectedItem(rowIndex, this.Text))
+                return;
+
+            switch (_referensiType)
+            {
+                case ReferencesType.NotaJualProduk:
+                    var itemJual = _listOfItemJual[rowIndex];
+                    this.Listener.Ok(this, itemJual);
+                    break;
+
+                case ReferencesType.NotaBeliProduk:
+                    var itemBeli = _listOfItemBeli[rowIndex];
+                    this.Listener.Ok(this, itemBeli);
+                    break;
+
+                default:
+                    break;
+            }
+
+            this.Close();
+        }
+
+        private void gridList_DoubleClick(object sender, EventArgs e)
+        {
+            if (base.IsButtonPilihEnabled)
+                Pilih();
+        }
+
+        private void gridList_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (KeyPressHelper.IsEnter(e))
+            {
+                if (base.IsButtonPilihEnabled)
+                    Pilih();
+            }
+        }
+    }
+}
