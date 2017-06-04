cf27e2996f3ca1a7b381f92fc2a2bde2c6d4ec3b
Kamarudin <rudi.krsoftware@gmail.com>
2017-06-04 08:00:18 +0700

    Penambahan dependency injection menggunakan ninject

---
+++ src/OpenRetail.WebAPI/App_Start/NinjectHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Ninject;
+
+namespace OpenRetail.WebAPI.App_Start
+{
+    public static class NinjectHelper
+    {
+        public static IKernel Kernel;
+    }
+}
