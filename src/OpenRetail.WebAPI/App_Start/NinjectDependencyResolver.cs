0fc4e3adaec6b349006cb7c64ca83ee3673a2c32
Kamarudin <rudi.krsoftware@gmail.com>
2017-06-04 08:00:18 +0700

    Penambahan dependency injection menggunakan ninject

---
+++ src/OpenRetail.WebAPI/App_Start/NinjectDependencyResolver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Ninject;
+using System.Web.Http.Dependencies;
+
+namespace OpenRetail.WebAPI.App_Start
+{
+    public class NinjectDependencyResolver : NinjectDependencyScope, IDependencyResolver
+    {
+        IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+            : base(kernel)
+        {
+            this.kernel = kernel;
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return new NinjectDependencyScope(kernel.BeginBlock());
+        }
+    }
+}
