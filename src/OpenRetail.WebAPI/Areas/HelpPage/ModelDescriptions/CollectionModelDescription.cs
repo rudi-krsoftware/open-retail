694c68aa285f622f8996e5d596735116c47351e1
Kamarudin <rudi.krsoftware@gmail.com>
2017-06-04 07:56:58 +0700

    Penambahan project web api

---
+++ src/OpenRetail.WebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace OpenRetail.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
