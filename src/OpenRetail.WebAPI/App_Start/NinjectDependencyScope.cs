ec316ce480437f0bb2713764206f84c74245f6c5
Kamarudin <rudi.krsoftware@gmail.com>
2017-06-04 08:00:18 +0700

    Penambahan dependency injection menggunakan ninject

---
+++ src/OpenRetail.WebAPI/App_Start/NinjectDependencyScope.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Ninject.Activation;
+using Ninject.Parameters;
+using Ninject.Syntax;
+using System.Web.Http.Dependencies;
+
+namespace OpenRetail.WebAPI.App_Start
+{
+    public class NinjectDependencyScope : IDependencyScope
+    {
+        protected IResolutionRoot resolver;
+
+        public NinjectDependencyScope(IResolutionRoot resolver)
+        {
+            this.resolver = resolver;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            if (resolver == null)
+                throw new ObjectDisposedException("this", "This scope has been disposed");
+
+            IRequest request = resolver.CreateRequest(serviceType, null, new Parameter[0], true, true);
+            return resolver.Resolve(request).SingleOrDefault();
+        }
+
+        public System.Collections.Generic.IEnumerable<object> GetServices(Type serviceType)
+        {
+            if (resolver == null)
+                throw new ObjectDisposedException("this", "This scope has been disposed");
+
+            IRequest request = resolver.CreateRequest(serviceType, null, new Parameter[0], true, true);
+            return resolver.Resolve(request).ToList();
+        }
+
+        public void Dispose()
+        {
+            IDisposable disposable = resolver as IDisposable;
+            if (disposable != null)
+                disposable.Dispose();
+
+            resolver = null;
+        }
+    }
+}
