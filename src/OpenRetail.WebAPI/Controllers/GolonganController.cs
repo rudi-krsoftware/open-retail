65250e9a658b7528c3262b54bb20b8acd3e4c133
Kamarudin <rudi.krsoftware@gmail.com>
2017-06-05 10:53:44 +0700

    Refactoring class GolonganController

--- src/OpenRetail.WebAPI/Controllers/GolonganController.cs
+++ src/OpenRetail.WebAPI/Controllers/GolonganController.cs
@@ -28,14 +28,24 @@ using OpenRetail.Model;
 using OpenRetail.Repository.Api;
 using OpenRetail.Repository.Service;
 using OpenRetail.WebAPI.Models;
+using OpenRetail.WebAPI.Models.DTO;
+using OpenRetail.WebAPI.Controllers.Helper;
 
 namespace OpenRetail.WebAPI.Controllers
 {
+    public interface IGolonganController : IBaseApiController<GolonganDTO>
+    {
+        IHttpActionResult GetByID(string id);
+        IHttpActionResult GetByName(string name, bool useLikeOperator = true);
+    }
+
     [RoutePrefix("api/golongan")]
-    public class GolonganController : ApiController
+    public class GolonganController : BaseApiController, IGolonganController
     {
         private IUnitOfWork _unitOfWork;
         private ILog _log;
+        private HttpStatusCode _httpStatusCode = HttpStatusCode.BadRequest;
+        private IHttpActionResult _response = null;
 
         public GolonganController(IUnitOfWork unitOfWork)
         {
@@ -48,23 +58,173 @@ namespace OpenRetail.WebAPI.Controllers
             this._log = log;
         }
 
+        [HttpGet, Route("get_by_id")]
+        public IHttpActionResult GetByID(string id)
+        {
+            _httpStatusCode = HttpStatusCode.BadRequest;
+            _response = Content(_httpStatusCode, new ResponsePackage(_httpStatusCode));
+
+            try
+            {
+                var results = new List<Golongan>();
+                var obj = _unitOfWork.GolonganRepository.GetByID(id);
+                
+                if (obj != null)
+                    results.Add(obj);
+
+                _httpStatusCode = HttpStatusCode.OK;
+                var output = GenerateOutput(_httpStatusCode, results);
+
+                _response = Content(_httpStatusCode, output);
+            }
+            catch (Exception ex)
+            {
+                if (_log != null)
+                    _log.Error("Error:", ex);
+            }
+
+            return _response;
+        }        
+
+        [HttpGet, Route("get_by_name")]
+        public IHttpActionResult GetByName(string name, bool useLikeOperator = true)
+        {
+            _httpStatusCode = HttpStatusCode.BadRequest;
+            _response = Content(_httpStatusCode, new ResponsePackage(_httpStatusCode));
+
+            try
+            {
+                var results = _unitOfWork.GolonganRepository.GetByName(name, useLikeOperator);
+
+                _httpStatusCode = HttpStatusCode.OK;
+                var output = GenerateOutput(_httpStatusCode, results);
+
+                _response = Content(_httpStatusCode, output);
+            }
+            catch (Exception ex)
+            {
+                if (_log != null)
+                    _log.Error("Error:", ex);
+            }
+
+            return _response;
+        }
+
         [HttpGet, Route("get_all")]
         public IHttpActionResult GetAll()
         {
-            IHttpActionResult response = null;
+            _httpStatusCode = HttpStatusCode.BadRequest;
+            _response = Content(_httpStatusCode, new ResponsePackage(_httpStatusCode));
 
-            var listOfGolongan = _unitOfWork.GolonganRepository.GetAll();
-
-            if (listOfGolongan.Count > 0)
+            try
             {
-                response = Content(HttpStatusCode.OK, new ResponsePackage(listOfGolongan));
+                var results = _unitOfWork.GolonganRepository.GetAll();
+
+                _httpStatusCode = HttpStatusCode.OK;
+                var output = GenerateOutput(_httpStatusCode, results);
+
+                _response = Content(_httpStatusCode, output);
             }
-            else
+            catch (Exception ex)
             {
-                response = Content(HttpStatusCode.InternalServerError, new ResponsePackage("Response does not contain any data."));
+                if (_log != null)
+                    _log.Error("Error:", ex);
             }
 
-            return response;
+            return _response;
         }
+
+        [HttpPost, Route("save")]
+        public IHttpActionResult Save(GolonganDTO objDTO)
+        {
+            _httpStatusCode = HttpStatusCode.BadRequest;
+            _response = Content(_httpStatusCode, new ResponsePackage(_httpStatusCode));
+
+            if (!objDTO.IsValidate(Request, ref _response))
+            {
+                return _response;
+            }
+
+            try
+            {
+                var obj = AutoMapper.Mapper.Map<Golongan>(objDTO);
+
+                var result = _unitOfWork.GolonganRepository.Save(obj);
+
+                _httpStatusCode = HttpStatusCode.OK;
+                var output = GenerateOutput(_httpStatusCode, result);
+
+                _response = Content(_httpStatusCode, output);
+            }
+            catch (Exception ex)
+            {
+                if (_log != null)
+                    _log.Error("Error:", ex);
+            }
+
+            return _response;
+        }
+
+        [HttpPost, Route("update")]
+        public IHttpActionResult Update(GolonganDTO objDTO)
+        {
+            _httpStatusCode = HttpStatusCode.BadRequest;
+            _response = Content(_httpStatusCode, new ResponsePackage(_httpStatusCode));
+
+            if (!objDTO.IsValidate(Request, ref _response))
+            {
+                return _response;
+            }
+
+            try
+            {
+                var obj = AutoMapper.Mapper.Map<Golongan>(objDTO);
+
+                var result = _unitOfWork.GolonganRepository.Update(obj);
+
+                _httpStatusCode = HttpStatusCode.OK;
+                var output = GenerateOutput(_httpStatusCode, result);
+
+                _response = Content(_httpStatusCode, output);
+            }
+            catch (Exception ex)
+            {
+                if (_log != null)
+                    _log.Error("Error:", ex);
+            }
+
+            return _response;
+        }
+
+        [HttpPost, Route("delete")]
+        public IHttpActionResult Delete(GolonganDTO objDTO)
+        {
+            _httpStatusCode = HttpStatusCode.BadRequest;
+            _response = Content(_httpStatusCode, new ResponsePackage(_httpStatusCode));
+
+            if (!objDTO.IsValidate(Request, ref _response))
+            {
+                return _response;
+            }
+
+            try
+            {
+                var obj = AutoMapper.Mapper.Map<Golongan>(objDTO);
+
+                var result = _unitOfWork.GolonganRepository.Delete(obj);
+
+                _httpStatusCode = HttpStatusCode.OK;
+                var output = GenerateOutput(_httpStatusCode, result);
+
+                _response = Content(_httpStatusCode, output);
+            }
+            catch (Exception ex)
+            {
+                if (_log != null)
+                    _log.Error("Error:", ex);
+            }
+
+            return _response;
+        }        
     }
 }
