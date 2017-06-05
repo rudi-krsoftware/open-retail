bb38b8cbb321fa377fbdf780c74fd71cc29fbf56
Kamarudin <rudi.krsoftware@gmail.com>
2017-06-05 10:55:48 +0700

    Penambahan class status response web api

---
+++ src/OpenRetail.WebAPI/Models/Status.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenRetail.WebAPI.Models
+{
+    public class Status
+    {
+        public Status()
+        {
+            Errors = new List<string>();
+        }
+
+        public int Code { get; set; }
+        public string Description { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}
