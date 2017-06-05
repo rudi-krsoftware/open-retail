f8393b78221257b09c916cd3c6a43c9a11ada804
Kamarudin <rudi.krsoftware@gmail.com>
2017-06-05 10:51:35 +0700

    Penambahan library nuget AutoMapper

---
+++ src/OpenRetail.WebAPI/AutoMapperConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using AutoMapper;
+using OpenRetail.Model;
+using OpenRetail.WebAPI.Models.DTO;
+
+namespace OpenRetail.WebAPI
+{
+    public class AutoMapperConfig
+    {
+        public static void Initialize()
+        {
+            Mapper.Initialize((config) =>
+            {
+                config.CreateMap<GolonganDTO, Golongan>();
+                config.CreateMap<JenisPengeluaranDTO, JenisPengeluaran>();
+            });
+        }
+    }
+}
