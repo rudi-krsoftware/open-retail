02fedad521b7c0cfeaf1beb4b9ec40e4e83f31c2
Kamarudin <rudi.krsoftware@gmail.com>
2017-06-05 11:05:27 +0700

    Penambahan informasi lisensi

--- src/OpenRetail.WebAPI/Controllers/Helper/FluentValidationExtensionMethod.cs
+++ src/OpenRetail.WebAPI/Controllers/Helper/FluentValidationExtensionMethod.cs
@@ -1,7 +1,21 @@
-﻿using FluentValidation;
-using FluentValidation.Internal;
-using FluentValidation.Results;
-using OpenRetail.WebAPI.Models;
+﻿/**
+ * Copyright (C) 2017 Kamarudin (http://coding4ever.net/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * The latest version of this file can be found at https://github.com/rudi-krsoftware/open-retail
+ */
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +25,11 @@ using System.Reflection;
 using System.Web;
 using System.Web.Hosting;
 using System.Web.Http;
+
+using FluentValidation;
+using FluentValidation.Internal;
+using FluentValidation.Results;
+using OpenRetail.WebAPI.Models;
 
 namespace OpenRetail.WebAPI.Controllers.Helper
 {
@@ -54,7 +73,7 @@ namespace OpenRetail.WebAPI.Controllers.Helper
             var et = obj.GetType();
             var evt = vt.MakeGenericType(et);
 
-            var assemblyModel = Assembly.GetExecutingAssembly(); // Assembly.LoadFrom(HostingEnvironment.MapPath("~/bin") + "/api.stamp.com.model.dll");
+            var assemblyModel = Assembly.GetExecutingAssembly();
 
             if (assemblyModel != null)
             {
