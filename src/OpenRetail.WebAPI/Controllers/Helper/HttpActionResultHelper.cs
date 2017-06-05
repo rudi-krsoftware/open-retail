48274abcd5e166c2bdf1164dc91b569c2db2e3b0
Kamarudin <rudi.krsoftware@gmail.com>
2017-06-05 10:52:38 +0700

    Penambahan class Helper FluentValidation

---
+++ src/OpenRetail.WebAPI/Controllers/Helper/HttpActionResultHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace OpenRetail.WebAPI.Controllers.Helper
+{
+    public class HttpActionResultHelper : IHttpActionResult
+    {
+        private HttpRequestMessage _request;
+        private HttpStatusCode _statusCode;
+        private object _message;
+
+        public HttpActionResultHelper(HttpRequestMessage request, HttpStatusCode statusCode, object message)
+        {
+            this._request = request;
+            this._statusCode = statusCode;
+            this._message = message;
+        }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_request.CreateResponse(_statusCode, _message));
+        }
+    }
+}
