8fca8c3e2b0b01edb842c7a0a9ed7213bc329b8e
Kamarudin <rudi.krsoftware@gmail.com>
2017-06-05 10:51:59 +0700

    Penambahan class BaseApiController

---
+++ src/OpenRetail.WebAPI/Controllers/BaseApiController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+using System.Net;
+using OpenRetail.WebAPI.Models;
+
+namespace OpenRetail.WebAPI.Controllers
+{
+    public class BaseApiController : ApiController
+    {
+        protected ResponsePackage GenerateOutput(HttpStatusCode httpStatusCode, object results)
+        {
+            var output = new ResponsePackage
+            {
+                Status = new Status
+                {
+                    Code = Convert.ToInt32(httpStatusCode),
+                    Description = httpStatusCode.ToString()
+                },
+                Results = results
+            };
+
+            return output;
+        }
+    }
+}
