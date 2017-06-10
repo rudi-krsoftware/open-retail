35dbd2103414eb40080233d7b18e71b534a317e8
Kamarudin <rudi.krsoftware@gmail.com>
2017-06-10 10:31:33 +0700

    Perbaikan bug: Menghapus string " -  -  - " ketika informasi kecamatan, kelurahan, kota dan kodePos kosong

--- src/OpenRetail.App/Transaksi/FrmPreviewLabelNotaPenjualan.cs
+++ src/OpenRetail.App/Transaksi/FrmPreviewLabelNotaPenjualan.cs
@@ -97,6 +97,8 @@ namespace OpenRetail.App.Transaksi
             var kepada1 = _customer.nama_customer;
             var kepada2 = _customer.alamat;
             var kepada3 = string.Format("{0} - {1} - {2} - {3}", kecamatan, kelurahan, kota, kodePos);
+            kepada3 = kepada3.Replace(" -  -  - ", "");
+
             var kepada4 = telepon;
 
             // info alamat kirim berdasarkan data alamat yang diedit pada saat penjualan
