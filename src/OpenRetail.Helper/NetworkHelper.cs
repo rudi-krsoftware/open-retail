1136eb06febc58a1c1312e9b280b3b8662bfb3a6
Kamarudin <rudi.krsoftware@gmail.com>
2017-11-24 16:42:10 +0700

    Penambahan class helper untuk mengecek koneksi internet

---
+++ src/OpenRetail.Helper/NetworkHelper.cs
@@ -0,0 +1,59 @@
+﻿/**
+ * Copyright (C) 2017 Kamarudin (http://coding4ever.net/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * The latest version of this file can be found at https://github.com/rudi-krsoftware/open-retail
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace OpenRetail.Helper
+{
+    public static class NetworkHelper
+    {
+        public static bool IsConnected(string ipAddress)
+        {
+            var result = false;
+
+            var pingSender = new Ping();
+            var options = new PingOptions();
+
+            // Use the default Ttl value which is 128,
+            // but change the fragmentation behavior.
+            options.DontFragment = true;
+
+            // Create a buffer of 32 bytes of data to be transmitted.
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
+            try
+            {
+                byte[] buffer = Encoding.ASCII.GetBytes(data);
+                int timeout = 120;
+
+                PingReply reply = pingSender.Send(ipAddress, timeout, buffer, options);
+
+                result = (reply.Status == IPStatus.Success);
+            }
+            catch
+            {
+            }
+
+            return result;
+        }
+    }
+}
