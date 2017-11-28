28968ee0577ddd2d5320c30e11d8a13fe32dba54
Kamarudin <rudi.krsoftware@gmail.com>
2017-11-28 11:35:09 +0700

    Penambahan sample data

---
+++ database/18_data-setting_aplikasi.sql
@@ -0,0 +1,26 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = public, pg_catalog;
+
+--
+-- Data for Name: m_setting_aplikasi; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY m_setting_aplikasi (setting_aplikasi_id, is_update_harga_jual_master_produk, is_stok_produk_boleh_minus) FROM stdin;
+1ef7cbcb-bf73-44be-aad2-b9d8e1b1a178	t	t
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
