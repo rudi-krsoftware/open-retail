b95a02a1e61fe1f212405074baf8aae47cc58e9a
Kamarudin <rudi.krsoftware@gmail.com>
2018-03-21 11:42:54 +0700

    Update struktur database

--- database/18_data-setting_aplikasi.sql
+++ database/18_data-setting_aplikasi.sql
@@ -15,8 +15,8 @@ SET search_path = public, pg_catalog;
 -- Data for Name: m_setting_aplikasi; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY m_setting_aplikasi (setting_aplikasi_id, is_update_harga_jual_master_produk, is_stok_produk_boleh_minus, is_fokus_input_kolom_jumlah) FROM stdin;
-1ef7cbcb-bf73-44be-aad2-b9d8e1b1a178	f	f	f
+COPY m_setting_aplikasi (setting_aplikasi_id, is_update_harga_jual_master_produk, is_stok_produk_boleh_minus, is_fokus_input_kolom_jumlah, is_tampilkan_keterangan_tambahan_item_jual, keterangan_tambahan_item_jual) FROM stdin;
+1ef7cbcb-bf73-44be-aad2-b9d8e1b1a178	f	t	f	f	Keterangan
 \.
 
 
